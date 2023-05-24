acab6705633308fc4300633d290331960a0d9a7f
Daniel Madsen <steamblacksej2@gmail.com>
2023-05-24 21:50:41 +0200

    Added new API key

--- app/src/main/java/dev/danieltm/weatherappv2/utilities/HttpRoutes.kt
+++ app/src/main/java/dev/danieltm/weatherappv2/utilities/HttpRoutes.kt
@@ -2,7 +2,7 @@ package dev.danieltm.weatherappv2.utilities
 
 object HttpRoutes {
     private const val BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
-    private const val API_KEY = "0d572d7e65d240ee14327ba7ac62d365"
+    private const val API_KEY = "f1e67f5406a03b5aad692e22031dc712"
 
     fun getWeatherFromLatAndLong(lat: String, long: String):String{
         val GET_WEATHER = "${BASE_URL}lat=$lat&lon=$long&lang=en&appid=${API_KEY}&units=metric"
